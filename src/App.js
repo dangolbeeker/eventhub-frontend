a559da55a4eb32999e66a904965f6953f2c93c86
Ramos159 <edwinramos269@gmail.com>
2019-08-17 13:33:08 -0400

    literally http to https... just web dev things. this should finally fix VE issue.

--- src/App.js
+++ src/App.js
@@ -38,12 +38,10 @@ class App extends React.Component{
     fetch('https://eventhub-backend.herokuapp.com/events/index')
     .then(resp=>resp.json())
     .then(events=>{this.props.addEvents(events)})
-    fetch('http://eventhub-backend.herokuapp.com/venue_events/index')
+    fetch('https://eventhub-backend.herokuapp.com/venue_events/index')
     .then(resp=>resp.json())
     .then(venueEvents=>{this.props.addVenueEvents(venueEvents)})
-    // fetch('https://eventhub-backend.herokuapp.com/reviews/index')
-    // .then(resp=>resp.json())
-    // .then(reviews=>{this.props.addReviews(reviews)})
+
     this.autologin()
   }
 
